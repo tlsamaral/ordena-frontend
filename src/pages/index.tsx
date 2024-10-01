0c48aedce061d4926f45c3b8ecc58392e8672da8
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-10-01 20:26:36 -0300

    feat: add login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,93 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { AuthContext } from '@/contexts/AuthContext'
+import { canSSRGuest } from '@/utils/canSSRGuest'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export const description =
-	"A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image.";
+  "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image."
+
+const SigInSchema = z.object({
+  email: z.string().email('Invalid email').min(1, 'Email is required'),
+  password: z.string().min(1, 'Password is required'),
+})
+
+type SigInScrema = z.infer<typeof SigInSchema>
 
 export default function Login() {
-	return (
-		<div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-			<div className="flex items-center justify-center py-12">
-				<div className="mx-auto grid w-[350px] gap-6">
-					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Acessar</h1>
-						<p className="text-balance text-muted-foreground">
-							Acesse digitando seu e-mail e sua senha abaixo
-						</p>
-					</div>
-					<div className="grid gap-4">
-						<div className="grid gap-2">
-							<Label htmlFor="email">Email</Label>
-							<Input
-								id="email"
-								type="email"
-								placeholder="m@example.com"
-								required
-							/>
-						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label htmlFor="password">Password</Label>
-								<Link
-									href="/forgot-password"
-									className="ml-auto inline-block text-sm underline"
-								>
-									Esqueceu sua senha?
-								</Link>
-							</div>
-							<Input id="password" type="password" required />
-						</div>
-						<Button type="submit" className="w-full">
-							Login
-						</Button>
-					</div>
-				</div>
-			</div>
-			<div className="hidden bg-muted lg:block">
-				<Image
-					src="/placeholder.svg"
-					alt="Image"
-					width="1920"
-					height="1080"
-					className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-				/>
-			</div>
-		</div>
-	);
+  const { signIn } = useContext(AuthContext)
+  const { register, handleSubmit } = useForm<SigInScrema>({
+    resolver: zodResolver(SigInSchema),
+  })
+
+  function handleSigIn(data: SigInScrema) {
+    try {
+      signIn(data)
+    } catch {
+      console.error('Error signing in')
+    }
+  }
+
+  return (
+    <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Acessar</h1>
+            <p className="text-balance text-muted-foreground">
+              Acesse digitando seu e-mail e sua senha abaixo
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(handleSigIn)} className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                {...register('email')}
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Esqueceu sua senha?
+                </Link>
+              </div>
+              <Input id="password" type="password" {...register('password')} />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  }
+})
