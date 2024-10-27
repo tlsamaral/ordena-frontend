95dbaf475a3d7b88218a2a678d7e5ab9158be461
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-10-27 20:51:31 -0300

    feat: add categories

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -20,16 +20,19 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { AddProduct } from "@/components/add-product";
+import { CategoriesTable } from "@/components/categories-table";
 import { ProductsTable } from "@/components/products-table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { GlobalContext } from "@/contexts/GlobalContext";
 import { api } from "@/services/apiClient";
+import type { Categories } from "@/types/category";
 import type { ProductList } from "@/types/product";
 import { canSSRAuth } from "@/utils/canSSRAuth";
 import { useContext, useEffect } from "react";
 
 export default function Products() {
-	const { products, setProducts } = useContext(GlobalContext);
+	const { products, setProducts, setCategories, categories } =
+		useContext(GlobalContext);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
@@ -37,6 +40,8 @@ export default function Products() {
 			try {
 				const response = await api.get<ProductList>("/product/all");
 				setProducts(response.data);
+				const categories = await api.get<Categories>("/category");
+				setCategories(categories.data);
 			} catch {
 				console.error("Error fetching products");
 			}
@@ -46,15 +51,11 @@ export default function Products() {
 
 	return (
 		<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-			<Tabs defaultValue="all">
+			<Tabs defaultValue="products">
 				<div className="flex items-center">
 					<TabsList>
-						<TabsTrigger value="all">All</TabsTrigger>
-						<TabsTrigger value="active">Active</TabsTrigger>
-						<TabsTrigger value="draft">Draft</TabsTrigger>
-						<TabsTrigger value="archived" className="hidden sm:flex">
-							Archived
-						</TabsTrigger>
+						<TabsTrigger value="products">Products</TabsTrigger>
+						<TabsTrigger value="categories">Categories</TabsTrigger>
 					</TabsList>
 					<div className="ml-auto flex items-center gap-2">
 						<DropdownMenu>
@@ -85,7 +86,7 @@ export default function Products() {
 						<AddProduct />
 					</div>
 				</div>
-				<TabsContent value="all">
+				<TabsContent value="products">
 					<Card x-chunk="dashboard-06-chunk-0">
 						<CardHeader>
 							<CardTitle>Products</CardTitle>
@@ -99,6 +100,24 @@ export default function Products() {
 						<CardFooter>
 							<div className="text-xs text-muted-foreground">
 								Showing <strong>1-10</strong> of <strong>32</strong> products
+							</div>
+						</CardFooter>
+					</Card>
+				</TabsContent>
+				<TabsContent value="categories">
+					<Card x-chunk="dashboard-06-chunk-0">
+						<CardHeader>
+							<CardTitle>Categories</CardTitle>
+							<CardDescription>
+								Manage your categories and view their sales performance.
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<CategoriesTable categories={categories} />
+						</CardContent>
+						<CardFooter>
+							<div className="text-xs text-muted-foreground">
+								Showing <strong>1-10</strong> of <strong>32</strong> categories
 							</div>
 						</CardFooter>
 					</Card>
