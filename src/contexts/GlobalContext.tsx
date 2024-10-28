a4973308c8d8e1205e6bf7fdc2e24a14cbd152d4
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-10-27 21:26:13 -0300

    feat: add categories on context

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,3 +1,4 @@
+import type { Categories } from "@/types/category";
 import type { Product } from "@/types/product";
 import {
 	type Dispatch,
@@ -10,6 +11,8 @@ import {
 type GlobalContextData = {
 	products: Product[];
 	setProducts: Dispatch<SetStateAction<Product[]>>;
+	categories: Categories;
+	setCategories: Dispatch<SetStateAction<Categories>>;
 };
 
 type GlobalProviderProps = {
@@ -20,9 +23,12 @@ export const GlobalContext = createContext({} as GlobalContextData);
 
 export function GlobalProvider({ children }: GlobalProviderProps) {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [categories, setCategories] = useState<Categories>([]);
 
 	return (
-		<GlobalContext.Provider value={{ products, setProducts }}>
+		<GlobalContext.Provider
+			value={{ products, setProducts, categories, setCategories }}
+		>
 			{children}
 		</GlobalContext.Provider>
 	);
