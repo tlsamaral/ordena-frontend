3d8a90cb464ddc159cbe1f8b481cc081cf5bb37f
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-02 12:10:01 -0300

    feat: dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -69,7 +69,6 @@ interface DashboardProps {
 }
 
 export default function Dashboard({ orders }: DashboardProps) {
-  console.log(orders)
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
