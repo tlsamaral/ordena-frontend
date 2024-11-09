24f3cfe45ccb47e961cc49416fc886039fd6cfa0
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-08 23:32:38 -0300

    feat: format distance with dayjs

---
+++ src/utils/formatDistanceToNow.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/pt-br'
+
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
+
+export const formatDistance = (date: string | Date) => {
+  return dayjs(new Date(date)).fromNow()
+}
