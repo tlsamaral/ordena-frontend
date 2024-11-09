286de24145e7f99f3e1bf24039589ce8c70db1b6
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-09 12:21:38 -0300

    add new order

--- src/pages/orders/new/[id].tsx
+++ src/pages/orders/new/[id].tsx
@@ -1,3 +1,7 @@
+import ProductCell from '@/components/product-cell'
+import { SearchCategory } from '@/components/search-category'
+import { SearchProduct } from '@/components/search-product'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -5,19 +9,65 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
 import { setupAPIClient } from '@/services/api'
-import type { OrderById, OrderResponse } from '@/types/order'
+import type { Categories, Category } from '@/types/category'
+import type { OrderById, ProductOrder } from '@/types/order'
+import type { Product, ProductList } from '@/types/product'
 import { canSSRAuth } from '@/utils/canSSRAuth'
+import { formatCurrency, formatValueToMoney } from '@/utils/formatCurrency'
+import { formatDistance } from '@/utils/formatDistanceToNow'
+import { Send } from 'lucide-react'
 import { notFound } from 'next/navigation'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 interface NewOrderPageProps {
   orderDetails: OrderById | null
+  categories: Categories
+  products: ProductList
 }
-export default function NewOrderPage({ orderDetails }: NewOrderPageProps) {
+
+export default function NewOrderPage({
+  orderDetails,
+  categories,
+  products,
+}: NewOrderPageProps) {
+  const [categorySelected, setCategorySelected] = useState('')
+  const [productsList, setProductsList] = useState<ProductOrder[]>([])
+
   if (orderDetails === null) {
     return notFound()
+  }
+
+  const handleSelectCategory = (category: Category) => {
+    setCategorySelected(category.id)
+  }
+
+  const handleSelectProduct = (product: Product) => {
+    setProductsList((prev) => [
+      ...prev,
+      {
+        id: product.id,
+        name: product.name,
+        amount: 1,
+        price: Number(product.price),
+      },
+    ])
+  }
+
+  const handleRemoveProduct = (product_id: string) => {
+    setProductsList((prev) =>
+      prev.filter((product) => product.id !== product_id),
+    )
+  }
+
+  let productsListed = products
+  if (categorySelected) {
+    productsListed = products.filter(
+      (product) => product.category_id === categorySelected,
+    )
   }
 
   return (
@@ -29,7 +79,140 @@ export default function NewOrderPage({ orderDetails }: NewOrderPageProps) {
             Nesta seção voce pode adicionar um pedido
           </CardDescription>
         </CardHeader>
-        <CardContent>{orderDetails.id}</CardContent>
+        <CardContent>
+          <div className="grid gap-8 py-4 w-auto">
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="price">Código do pedido</Label>
+                </div>
+              </div>
+              <div className="col-span-3">
+                <Input
+                  className="col-span-3"
+                  type="text"
+                  value={orderDetails.id}
+                  disabled
+                />
+              </div>
+            </div>
+            <Separator />
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="name">Nome</Label>
+                </div>
+              </div>
+              <div className="col-span-3">
+                <Input
+                  className="col-span-3"
+                  type="text"
+                  value={orderDetails.name}
+                  disabled
+                />
+              </div>
+            </div>
+            <Separator />
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="name">Informações</Label>
+                  <span className="text-xs text-neutral-100/80">
+                    Informações como status e tempo de criação
+                  </span>
+                </div>
+              </div>
+              <div className="col-span-3 grid grid-cols-2 gap-4">
+                <Input
+                  className="col-span-1"
+                  type="text"
+                  value={orderDetails ? 'Aberto' : 'Fechado'}
+                  disabled
+                />
+                <Input
+                  className="col-span-1"
+                  type="text"
+                  value={formatDistance(orderDetails.created_at)}
+                  disabled
+                />
+              </div>
+            </div>
+            <Separator />
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="cateory">Categoria de produtos</Label>
+                  <span className="text-xs text-neutral-100/80">
+                    Selecione a categoria para filtrar produtos
+                  </span>
+                </div>
+              </div>
+              <div className="col-span-3">
+                <SearchCategory
+                  categories={categories}
+                  categoryIdSelected={categorySelected}
+                  onCategoryChange={handleSelectCategory}
+                  className="w-full"
+                  isRegister={false}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="cateory">Categoria de produtos</Label>
+                  <span className="text-xs text-neutral-100/80">
+                    Selecione a categoria para filtrar produtos
+                  </span>
+                </div>
+              </div>
+              <div className="col-span-3">
+                <SearchProduct
+                  products={products}
+                  onProductChange={handleSelectProduct}
+                  productsListed={productsList}
+                  className="w-full"
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-4 items-start gap-4">
+              <div className="col-span-1 flex flex-col justify-start gap-2">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="cateory">Listagem de produtos</Label>
+                  <span className="text-xs text-neutral-100/80">
+                    Ao lado a lista de produtos selecionados
+                  </span>
+                </div>
+              </div>
+              <div className="col-span-3">
+                {productsList.length > 0 ? (
+                  <div className="flex flex-col gap-2">
+                    {productsList.map((product) => (
+                      <ProductCell
+                        product={product}
+                        key={product.id}
+                        setProductsList={setProductsList}
+                        handleRemoveProduct={handleRemoveProduct}
+                        productList={products}
+                      />
+                    ))}
+                  </div>
+                ) : (
+                  <p>Nennhum produto adicionado</p>
+                )}
+              </div>
+            </div>
+            <Separator />
+            <div className="flex justify-end gap-4">
+              <Button type="button" size="sm" variant="destructive">
+                Cancelar
+              </Button>
+              <Button type="button" size="sm">
+                Enviar pedido <Send size={16} className="text-sm ml-1" />
+              </Button>
+            </div>
+          </div>
+        </CardContent>
       </Card>
     </main>
   )
@@ -39,11 +222,15 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
   const { id } = ctx.query
 
   const apiClient = setupAPIClient(ctx)
-  const response = await apiClient.get<OrderById>(`/order/${id}`)
+  const getOrder = await apiClient.get<OrderById>(`/order/${id}`)
+  const getCategories = await apiClient.get<Categories>('/category')
+  const getProducts = await apiClient.get<ProductList>('/product/all')
 
   return {
     props: {
-      orderDetails: response.data,
+      orderDetails: getOrder.data,
+      categories: getCategories.data,
+      products: getProducts.data,
     },
   }
 })
