91ad230e29978acd9312c31bb869b9fe444bb09b
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-15 17:19:58 -0300

    fix: remove imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,27 @@
-import { Layout } from "@/components/layout";
-import { Toaster } from "@/components/ui/sonner";
-import { AuthProvider } from "@/contexts/AuthContext";
-import { GlobalProvider } from "@/contexts/GlobalContext";
-import "@/styles/globals.css";
-import { ThemeProvider, useTheme } from "next-themes";
-import type { AppProps } from "next/app";
+import { Layout } from '@/components/layout'
+import { Toaster } from '@/components/ui/sonner'
+import { AuthProvider } from '@/contexts/AuthContext'
+import { GlobalProvider } from '@/contexts/GlobalContext'
+import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="dark"
-			enableSystem
-			disableTransitionOnChange
-		>
-			<AuthProvider>
-				<GlobalProvider>
-					<Layout>
-						<Toaster />
-						<Component {...pageProps} />
-					</Layout>
-				</GlobalProvider>
-			</AuthProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <AuthProvider>
+        <GlobalProvider>
+          <Layout>
+            <Toaster />
+            <Component {...pageProps} />
+          </Layout>
+        </GlobalProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
