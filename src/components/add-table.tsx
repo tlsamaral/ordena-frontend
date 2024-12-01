71d63438d27611d0b3de18d819a256133d463f57
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-12-01 14:43:58 -0300

    feat: translate

--- src/components/add-table.tsx
+++ src/components/add-table.tsx
@@ -66,7 +66,7 @@ export function AddTable() {
 					<div className="grid gap-4 py-4">
 						<div className="grid grid-cols-4 items-center gap-4">
 							<Label htmlFor="name" className="text-right">
-								Name
+								Nome
 							</Label>
 							<Input
 								id="name"
