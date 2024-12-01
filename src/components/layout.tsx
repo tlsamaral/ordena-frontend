3d4a4cb3df391787d93b887b7331ecd645421716
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-12-01 10:58:42 -0300

    add signup in white list

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,23 +3,23 @@ import { Aside } from './aside'
 import { Header } from './header'
 
 interface LayoutProps {
-  children: React.ReactNode
+	children: React.ReactNode
 }
 export function Layout({ children }: LayoutProps) {
-  const router = useRouter()
-  const publicRoutes = ['/', '/login', '/404']
+	const router = useRouter()
+	const publicRoutes = ['/', '/login', '/404', '/signup']
 
-  if (publicRoutes.includes(router.pathname)) {
-    return <>{children}</>
-  }
-  return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <Aside />
+	if (publicRoutes.includes(router.pathname)) {
+		return <>{children}</>
+	}
+	return (
+		<div className="flex min-h-screen w-full flex-col bg-muted/40">
+			<Aside />
 
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <Header />
-        {children}
-      </div>
-    </div>
-  )
+			<div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+				<Header />
+				{children}
+			</div>
+		</div>
+	)
 }
