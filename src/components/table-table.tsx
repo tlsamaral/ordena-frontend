0642fe5d6f4f8159e89169155004abbec7a07a20
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-12-01 10:52:30 -0300

    feat: tables

---
+++ src/components/table-table.tsx
@@ -0,0 +1,259 @@
+import {
+	type ColumnDef,
+	type ColumnFiltersState,
+	type SortingState,
+	type VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
+} from '@tanstack/react-table'
+import * as React from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+import { GlobalContext } from '@/contexts/GlobalContext'
+import { api } from '@/services/apiClient'
+import type { Table as TableData } from '@/types/table'
+import type { User, Users } from '@/types/user'
+import { ChevronDown, ChevronsUpDown, EllipsisVertical, X } from 'lucide-react'
+import { toast } from 'sonner'
+import { AddTable } from './add-table'
+import { Badge } from './ui/badge'
+
+export const columns: ColumnDef<TableData>[] = [
+	{
+		id: 'select',
+		header: ({ table }) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && 'indeterminate')
+				}
+				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+				aria-label="Select all"
+			/>
+		),
+		cell: ({ row }) => (
+			<Checkbox
+				checked={row.getIsSelected()}
+				onCheckedChange={(value) => row.toggleSelected(!!value)}
+				aria-label="Select row"
+			/>
+		),
+		enableSorting: false,
+		enableHiding: false,
+	},
+	{
+		accessorKey: 'name',
+		header: 'Nome',
+		cell: ({ row }) => {
+			return <div className="capitalize">{row.getValue('name')}</div>
+		},
+	},
+	{
+		accessorKey: 'id',
+		header: 'Código',
+		cell: ({ row }) => {
+			return (
+				<div className="capitalize flex items-center gap-2">
+					{row.getValue('id')}
+				</div>
+			)
+		},
+	},
+	{
+		id: 'actions',
+		enableHiding: false,
+		cell: ({ row }) => {
+			const { setTables } = React.useContext(GlobalContext)
+			const table = row.original
+
+			const deleteTable = async () => {
+				try {
+					// await api.delete(`/users/${table.id}`)
+					toast('Mesa deletado', {
+						description: `${table.name} foi deletada com sucesso.`,
+						icon: '😶',
+					})
+					setTables((prev) => prev.filter((u) => u.id !== table.id))
+				} catch (err) {
+					console.error(err)
+					toast('', {
+						icon: <X size={14} />,
+						className:
+							'destructive group border-destructive bg-destructive text-destructive-foreground',
+						description: 'Ocorreu um erro ao deletar esta mesa.',
+					})
+				}
+			}
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<EllipsisVertical className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Ações</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<Button
+								className="h-7 w-full"
+								variant="destructive"
+								onClick={deleteTable}
+							>
+								Delete
+							</Button>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
+	},
+]
+
+interface TablesProps {
+	tables: TableData[]
+}
+
+export function TablesTable({ tables }: TablesProps) {
+	const [sorting, setSorting] = React.useState<SortingState>([])
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[],
+	)
+	const [columnVisibility, setColumnVisibility] =
+		React.useState<VisibilityState>({})
+	const [rowSelection, setRowSelection] = React.useState({})
+
+	const table = useReactTable({
+		data: tables,
+		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection,
+		},
+	})
+
+	return (
+		<div className="w-full">
+			<div className="flex items-center py-4 justify-between">
+				<Input
+					placeholder="Filtrar mesas..."
+					value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+					onChange={(event) =>
+						table.getColumn('name')?.setFilterValue(event.target.value)
+					}
+					className="max-w-sm h-8"
+				/>
+				<AddTable />
+			</div>
+			<div className="rounded-md border">
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext(),
+													)}
+										</TableHead>
+									)
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && 'selected'}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext(),
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<div className="flex items-center justify-end space-x-2 py-4">
+				<div className="flex-1 text-sm text-muted-foreground">
+					{table.getFilteredSelectedRowModel().rows.length} of{' '}
+					{table.getFilteredRowModel().rows.length} row(s) selected.
+				</div>
+				<div className="space-x-2">
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => table.previousPage()}
+						disabled={!table.getCanPreviousPage()}
+					>
+						Previous
+					</Button>
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => table.nextPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						Next
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
+}
