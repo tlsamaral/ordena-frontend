cc5af7081383c20365e46175cb94d6dbf57d10cf
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-12-01 10:54:26 -0300

    feat: add tables at contexty

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,35 +1,52 @@
-import type { Categories } from "@/types/category";
-import type { Product } from "@/types/product";
+import type { Categories } from '@/types/category'
+import type { Product } from '@/types/product'
+import type { Table } from '@/types/table'
+import type { User } from '@/types/user'
 import {
 	type Dispatch,
 	type ReactNode,
 	type SetStateAction,
 	createContext,
 	useState,
-} from "react";
+} from 'react'
 
 type GlobalContextData = {
-	products: Product[];
-	setProducts: Dispatch<SetStateAction<Product[]>>;
-	categories: Categories;
-	setCategories: Dispatch<SetStateAction<Categories>>;
-};
+	products: Product[]
+	setProducts: Dispatch<SetStateAction<Product[]>>
+	categories: Categories
+	setCategories: Dispatch<SetStateAction<Categories>>
+	users: User[]
+	setUsers: Dispatch<SetStateAction<User[]>>
+	tables: Table[]
+	setTables: Dispatch<SetStateAction<Table[]>>
+}
 
 type GlobalProviderProps = {
-	children: ReactNode;
-};
+	children: ReactNode
+}
 
-export const GlobalContext = createContext({} as GlobalContextData);
+export const GlobalContext = createContext({} as GlobalContextData)
 
 export function GlobalProvider({ children }: GlobalProviderProps) {
-	const [products, setProducts] = useState<Product[]>([]);
-	const [categories, setCategories] = useState<Categories>([]);
+	const [products, setProducts] = useState<Product[]>([])
+	const [categories, setCategories] = useState<Categories>([])
+	const [users, setUsers] = useState<User[]>([])
+	const [tables, setTables] = useState<Table[]>([])
 
 	return (
 		<GlobalContext.Provider
-			value={{ products, setProducts, categories, setCategories }}
+			value={{
+				products,
+				setProducts,
+				categories,
+				setCategories,
+				users,
+				setUsers,
+				tables,
+				setTables,
+			}}
 		>
 			{children}
 		</GlobalContext.Provider>
-	);
+	)
 }
